5b6b497310399b6076d7ba26e2386cf1b5a70b95
Isaac Correia Gonçalves <82903174+isaac-goncalves@users.noreply.github.com>
2022-10-04 10:01:29 -0300

    -

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,7 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { toast } from 'react-toastify';
-import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { createContext, ReactNode, useContext, useState } from "react";
+import { toast } from "react-toastify";
+import { api } from "../services/api";
+import { Product, Stock } from "../types";
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,21 +23,54 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem("@RocketShoes:cart");
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
-    return [];
+    return [
+      {
+        id: 1,
+        title: "Tênis de Caminhada Leve Confortável",
+        price: 179.9,
+        image:
+          "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
+        amount: 1,
+      },
+      {
+        id: 2,
+        title: "Tênis de Caminhada Leve Confortável",
+        price: 179.9,
+        image:
+          "https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg",
+        amount: 1,
+      },
+    ];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
-    }
+      console.log(productId);
+
+      const productAlreadyExistOnCart = cart.find(({ id }) => id === productId);
+
+      if (productAlreadyExistOnCart) {
+        console.log("ja existe no carrinho!");
+
+        //somar e guardar adicionando o amount
+        const total = productAlreadyExistOnCart.amount + 1;
+
+        const { data } = await api.get<Stock>(`stock/${productId}`);
+
+        if (total > data.amount) {
+          toast.error("Quantidade solicitada fora de estoque");
+        }
+
+        console.log(data);
+      }
+      // guardar adicionando o amount
+    } catch {}
   };
 
   const removeProduct = (productId: number) => {
